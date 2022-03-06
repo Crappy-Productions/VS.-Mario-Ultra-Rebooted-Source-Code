0673bcb83ec27d881cc2cad951f9093616c2192d
Crappy Productions <mariocrappypro@gmail.com>
2022-03-06 15:27:09 +0800

    .

--- example_mods/stages/Faker Stage.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-
-     makeLuaSprite('fakebg','fakebg',-1200,-400)
-     setLuaSpriteScrollFactor('fakebg', 1.0, 1.0);
-     scaleLuaSprite('fakebg', 0.8, 0.8);
-
-	 addLuaSprite('fakebg',false) 
-end
